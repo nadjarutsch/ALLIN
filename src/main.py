e359b4b1980de7c7c08aea321aed36d2fb04635a
nadjarutsch <nadja.rutsch@gmx.de>
2022-06-01 14:25:13 +0200

    depcon w/o threshold

--- src/main.py
+++ src/main.py
@@ -84,7 +84,7 @@ def main(cfg: DictConfig):
     
     for seed in seeds:
         config['seed'] = seed
-        run = wandb.init(project="idiod", entity="nadjarutsch", group='depcon kmeans', notes='with clustering metrics', tags=['fci', 'kmeans'], config=config, reinit=True)
+        run = wandb.init(project="idiod", entity="nadjarutsch", group='depcon kmeans w/o threshold', notes='with clustering metrics', tags=['fci', 'kmeans'], config=config, reinit=True)
         with run:
             # generate data
             dag = data_gen.generate_dag(num_vars=config['num_vars'], edge_prob=config['edge_prob'], fns='linear gaussian', mu=config['mu'], sigma=config['sigma'])
