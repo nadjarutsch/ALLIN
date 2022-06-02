3eeba9600937c392b2c8b75bf98f52c80fb34627
nadjarutsch <nadja.rutsch@gmx.de>
2022-06-02 16:51:20 +0200

    fix char issue

--- src/clustering/kmeans.py
+++ src/clustering/kmeans.py
@@ -2,7 +2,7 @@ from sklearn.cluster import KMeans
 import numpy as np
 
 
-def kmeans(X, n_clusters, init=‘k-means++’, n_init=10):
+def kmeans(X, n_clusters, init='k-means++', n_init=10):
     clustering = KMeans(n_clusters=n_clusters, init=init, n_init=n_init).fit(X)
 #    labels = clustering.labels_
 
