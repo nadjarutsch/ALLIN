98f1854e4ac3a4dad1b933ac2248016d2cd5a18d
nadjarutsch <nadja.rutsch@gmx.de>
2022-06-26 22:47:40 +0200

    extensive experiments

--- src/main.py
+++ src/main.py
@@ -95,7 +95,7 @@ def main(cfg: DictConfig):
     
     for seed in seeds:
         config['seed'] = seed
-        run = wandb.init(project="idiod", entity="nadjarutsch", group='extensive experiments clustering & causal discovery', notes='', tags=['kmeans', 'jci', 'pc'], config=config, reinit=True)
+        run = wandb.init(project="idiod", entity="nadjarutsch", group='depcon extensive experiments clustering & causal discovery', notes='', tags=['kmeans', 'jci', 'pc', 'depcon'], config=config, reinit=True)
         with run:
             # generate data
             dag = data_gen.generate_dag(num_vars=config['num_vars'], edge_prob=config['edge_prob'], fns='linear gaussian', mu=config['mu'], sigma=config['sigma'])
