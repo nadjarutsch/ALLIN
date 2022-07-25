bd8be9d53fdddc8f6b157d71d30c3374459bf859
nadjarutsch <nadja.rutsch@gmx.de>
2022-07-25 19:41:23 +0200

    Kmeans cd w feature transformation

--- src/main.py
+++ src/main.py
@@ -142,8 +142,8 @@ def main(cfg: DictConfig):
             fit_adj_matrix.fill_diagonal_(0)
 
             ood.fit(synth_dataset.partitions[0], gnmodel, loss, optimizer, config["fit_epochs"], config["batch_size"], fit_adj_matrix)
-            preds = gnmodel(synth_dataset.features[..., -1], fit_adj_matrix)
-            losses = loss(preds, synth_dataset.features[..., -1])
+            preds = gnmodel(synth_dataset.features[..., :-1], fit_adj_matrix)
+            losses = loss(preds, synth_dataset.features[..., :-1])
 
             true_adj_matrix = nx.to_numpy_array(true_graph)
             root_vars = torch.nonzero(torch.all(~true_adj_matrix.bool(), dim=1))
