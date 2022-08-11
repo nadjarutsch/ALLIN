bug fix: normalizing data

src/causal_discovery/prepare_data.py
@@ -16,32 +16,13 @@ def prepare_for_pc(data: data.PartitionData, variables: list[str]) -> pd.DataFra
     data.set_random_intervention_targets()
     cols_int = ['I_%s' % i for i in range(len(data.partitions))]
 
-    if data.memberships is None:
-        dfs = []
-        for partition, target in zip(data.partitions, data.intervention_targets): # if no intervention targets were explicitly set, data.intervention_targets consists of only one list with zeros
-             df_data = partition.features[...,:-1].clone().numpy()
-             df = pd.DataFrame(df_data)
-             df = (df - df.mean()) / df.std()  # normalize
+    if len(data.features) != len(data.memberships):
+        data.features = data.features[data.labels >= 0]
+    df = pd.DataFrame(data.features[...,:-1].clone().numpy())
+    df = (df - df.mean()) / df.std()  # normalize
+    df.columns = variables
+    df[cols_int] = data.memberships
 
-             df.columns = variables
-             df[cols_int] = target.expand(partition.features.shape[0], len(data.partitions)).clone().numpy()
-
-             # df = (df - df.mean()) / df.std()  # normalize
-
-             dfs.append(df)
-
-        df = pd.concat(dfs)
-    # use membership attribute in data object
-    else:
-        if len(data.features) != len(data.memberships):
-            data.features = data.features[data.labels >= 0]
-        df = pd.DataFrame(data.features[...,:-1].clone().numpy())
-        df = (df - df.mean()) / df.std()  # normalize
-        df.columns = variables
-        df[cols_int] = data.memberships
-    # drop data points without inferred intervention target
- #   drop_indices = ((df[cols_int] == 1).sum(axis=1) == 0).index[((df[cols_int] == 1).sum(axis=1) == 0)]
- #   df.drop(drop_indices)
     # re-order dataframe columns 
     cols = variables + cols_int
     df = df[cols]    