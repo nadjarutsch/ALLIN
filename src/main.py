eeeffa65457637a6c5b2074333de330587bf17ed
nadjarutsch <nadja.rutsch@gmx.de>
2022-08-14 00:53:54 +0200

    20 variables

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ import hdbscan
 
 
 
-os.environ['WANDB_MODE'] = 'offline'
+# os.environ['WANDB_MODE'] = 'offline'
 
 @hydra.main(config_path="./config", config_name="config")
 def main(cfg: DictConfig):
