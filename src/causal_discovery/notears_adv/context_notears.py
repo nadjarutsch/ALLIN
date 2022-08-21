80fab23d39da85f76740c0f540bf0c772811fa4f
nadjarutsch <nadja.rutsch@gmx.de>
2022-08-21 19:20:29 +0200

    notears+context

---
+++ src/causal_discovery/notears_adv/context_notears.py
@@ -0,0 +1,106 @@
+import numpy as np
+import scipy.linalg as slin
+import scipy.optimize as sopt
+from scipy.special import expit as sigmoid
+
+
+def context_notears_linear(X,
+                           lambda1,
+                           num_vars,
+                           max_iter=1000,
+                           h_tol=1e-8,
+                           rho_max=1e+16,
+                           w_threshold=0.3):
+    """Solve min_W L(W; X) + lambda1 ‖W‖_1 s.t. h(W) = 0 using augmented Lagrangian.
+    Args:
+        X (np.ndarray): [n, d] sample matrix
+        lambda1 (float): l1 penalty parameter
+        loss_type (str): l2, logistic, poisson
+        max_iter (int): max num of dual ascent steps
+        h_tol (float): exit if |h(w_est)| <= htol
+        rho_max (float): exit if rho >= rho_max
+        w_threshold (float): drop edge if |weight| < threshold
+    Returns:
+        W_est (np.ndarray): [d, d] estimated DAG
+    """
+    def _loss(U,V):
+        """Evaluate value and gradient of loss."""
+        M = (X_in * sigmoid(C @ V)) @ U # TODO: check order
+        R = X_in - M
+        loss = 0.5 / X.shape[0] * (R ** 2).sum()
+        G_loss_U = - 1.0 / X.shape[0] * (X_in * sigmoid(C @ V)).T @ R
+        G_loss_V = - 1.0 / X.shape[0] * C.T @ (X_in * sigmoid_der(C @ V) * (R @ U.T))
+        #G_loss_V = - 1.0 / X.shape[0] * np.einsum('kl,dl->kl', C.T @ (R * X_in), U)
+     #   G_loss = np.concatenate((G_loss_U, G_loss_V), axis=1)
+
+        return loss, G_loss_U, G_loss_V
+
+    def sigmoid_der(X):
+        return sigmoid(X) * (1 - sigmoid(X))
+
+    def _h(W):
+        """Evaluate value and gradient of acyclicity constraint."""
+     #   U, V = W[:num_vars, :num_vars], W[num_vars:, :num_vars]
+        E = slin.expm(W * W)  # (Zheng et al. 2018)
+        h = np.trace(E) - d
+        #     # A different formulation, slightly faster at the cost of numerical stability
+        #     M = np.eye(d) + U * U / d  # (Yu et al. 2019)
+        #     E = np.linalg.matrix_power(M, d - 1)
+        #     h = (E.T * M).sum() - d
+        G_h = E.T * W * 2
+        return h, G_h
+
+    def _adj(w, dim1, dim2):
+        """Convert doubled variables ([2 d^2] array) back to original variables ([d, d] matrix)."""
+        return (w[:dim1 * dim2] - w[dim1 * dim2:]).reshape([dim1, dim2])
+
+    def _func(w):
+        """Evaluate value and gradient of augmented Lagrangian for doubled variables ([2 d^2] array)."""
+        u,v = w[:2 * num_vars * num_vars], w[2 * num_vars * num_vars:]
+        U,V = _adj(u, dim1=num_vars, dim2=num_vars), _adj(v, dim1=d-num_vars, dim2=num_vars) #v.reshape([d-num_vars, num_vars])
+        loss, G_loss_U, G_loss_V = _loss(U, V)
+        h, G_h = _h(U)
+        obj = loss + 0.5 * rho * h * h + alpha * h + lambda1 * u.sum()
+       # temp = np.zeros_like(G_loss_U)
+      #  temp =
+        G_smooth_U = G_loss_U + (rho * h + alpha) * G_h
+        G_U = np.concatenate((G_smooth_U + lambda1, - G_smooth_U + lambda1), axis=None)
+        G_V = np.concatenate((G_loss_V + lambda1, - G_loss_V + lambda1), axis=None)
+        # G_V = G_loss_V.flatten()
+        g_obj = np.concatenate((G_U, G_V))
+        return obj, g_obj
+
+    X_in, C = X[:, :num_vars], X[:, num_vars:]
+    n, d = X.shape # TODO: add dimension?
+   # d1, d2 = num_vars, d - num_vars
+
+    u_est, rho, alpha, h = np.random.normal(loc=0.0, scale=0.1, size=(2*num_vars*num_vars)), 1.0, 0.0, np.inf
+    v_est = np.random.normal(loc=1.0, size=(2 * (d - num_vars) * num_vars))
+    # w_est, rho, alpha, h = np.random.normal(size=(2 * d * d)), 1.0, 0.0, np.inf  # double w_est into (w_pos, w_neg)
+    w_est = np.concatenate((u_est, v_est))
+    bnds = [(0, 0) if i == j else (0, None) for _ in range(2) for i in range(num_vars) for j in range(num_vars)]
+    bnds.extend([(None, None) for _ in range(2) for i in range(d - num_vars) for j in range(num_vars)])
+
+    X_in = X_in - np.mean(X_in, axis=0, keepdims=True) # normalize input (zero-center)
+    for _ in range(max_iter):
+        w_new, h_new = None, None
+        while rho < rho_max:
+            sol = sopt.minimize(_func, w_est, method='L-BFGS-B', jac=True, bounds=bnds)
+            w_new = sol.x
+            h_new, _ = _h(_adj(w_new[:2 * num_vars * num_vars], dim1=num_vars, dim2=num_vars))
+            if h_new > 0.25 * h:
+                rho *= 10
+            else:
+                break
+        w_est, h = w_new, h_new
+        alpha += rho * h
+        if h <= h_tol or rho >= rho_max:
+            break
+
+    U_est = _adj(w_new[:2 * num_vars * num_vars], dim1=num_vars, dim2=num_vars)
+    # V_est = w_new[2*num_vars*num_vars:].reshape([d-num_vars,num_vars])
+    V_est = _adj(w_new[2 * num_vars * num_vars:], dim1=d-num_vars, dim2=num_vars)
+    U_est[np.abs(U_est) < w_threshold] = 0
+    # V_est[np.abs(V_est) < w_threshold] = 0
+    return U_est, V_est
+
--- src/causal_discovery/notears_model.py
+++ src/causal_discovery/notears_model.py
@@ -1,10 +1,12 @@
 from causal_discovery.notears.linear import *
+from causal_discovery.notears_adv.context_notears import *
 import networkx as nx
+from scipy.special import expit as sigmoid
 
-class Notears():
+class Notears:
     def __init__(self,
                  lambda1,
-                 loss_type,
+                 loss_type="l2",
                  max_iter=100,
                  h_tol=1e-8,
                  rho_max=1e+16,
@@ -20,19 +22,51 @@ class Notears():
     def predict(self, cd_input: tuple):
 
         variables, data = cd_input
-        U_est, V_est = notears_linear(X = data,
-                                   lambda1 = self.lambda1,
-                                   loss_type = self.loss_type,
-                                   num_vars = len(variables),
-                                   max_iter = self.max_iter,
-                                   h_tol = self.h_tol,
-                                   rho_max = self.rho_max,
-                                   w_threshold = self.w_threshold)
+        W_est = notears_linear(X=data,
+                               lambda1=self.lambda1,
+                               loss_type=self.loss_type,
+                               max_iter=self.max_iter,
+                               h_tol=self.h_tol,
+                               rho_max=self.rho_max,
+                               w_threshold=self.w_threshold)
 
-        A_est = np.zeros((data.shape[1], data.shape[1]))
-        A_est[:len(variables),:len(variables)] = U_est != 0
-        A_est[len(variables):,:len(variables)] = V_est < 0
+        A_est = W_est != 0
         pred_graph = nx.from_numpy_array(A_est, create_using=nx.DiGraph)
         mapping = dict(zip(range(len(variables)), variables))
 
         return nx.relabel_nodes(pred_graph, mapping)
+
+
+
+class NotearsContext(Notears):
+    def __init__(self,
+                 lambda1,
+                 loss_type="l2",
+                 max_iter=100,
+                 h_tol=1e-8,
+                 rho_max=1e+16,
+                 w_threshold=0.3,
+                 v_threshold=0.3):
+        super().__init__(lambda1, loss_type, max_iter, h_tol, rho_max, w_threshold)
+        self.v_threshold = v_threshold
+
+    def predict(self, cd_input: tuple):
+
+        variables, data = cd_input
+        U_est, V_est = context_notears_linear(X=data,
+                                              lambda1=self.lambda1,
+                                              num_vars=len(variables),
+                                              max_iter=self.max_iter,
+                                              h_tol=self.h_tol,
+                                              rho_max=self.rho_max,
+                                              w_threshold=self.w_threshold)
+
+        C = np.eye(data.shape[1] - len(variables))
+        A_est = np.zeros((data.shape[1], data.shape[1]))
+        A_est[:len(variables), :len(variables)] = U_est != 0
+        A_est[len(variables):, :len(variables)] = sigmoid(C @ V_est) > self.v_threshold
+        pred_graph = nx.from_numpy_array(A_est, create_using=nx.DiGraph)
+        mapping = dict(zip(range(len(variables)), variables))
+
+        return nx.relabel_nodes(pred_graph, mapping)
+
