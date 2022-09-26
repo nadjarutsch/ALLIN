6600885f2cc08c16e950c8461c2f80c235c4afa3
nadjarutsch <nadja.rutsch@gmx.de>
2022-09-26 17:32:21 +0200

    circular graph layout

--- src/plotting.py
+++ src/plotting.py
@@ -8,7 +8,7 @@ def plot_graph(graph, title, savefig=True):
     visual.set_edge_attributes(graph)
     plt.figure(figsize=(6, 6))
     colors = visual.get_colors(graph)
-    nx.draw(graph, with_labels=True, node_size=1000, node_color='w', edgecolors='black', edge_color=colors)
+    nx.draw(graph, pos=nx.circular_layout(graph), with_labels=True, node_size=1000, node_color='w', edgecolors='black', edge_color=colors)
     if savefig:
         plt.savefig(f"{title}.pdf")
     wandb.log({title: wandb.Image(plt)})
