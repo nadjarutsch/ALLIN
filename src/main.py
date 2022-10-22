4f7d6d6d72d8cc0411d40258813fc91c8f572f83
nadjarutsch <nadja.rutsch@gmx.de>
2022-10-22 19:27:41 +0200

    wandb logging

--- src/main.py
+++ src/main.py
@@ -38,8 +38,8 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 
-# os.environ['HYDRA_FULL_ERROR'] = '1'
-# os.environ['WANDB_MODE'] = 'offline'
+#os.environ['HYDRA_FULL_ERROR'] = '1'
+#os.environ['WANDB_MODE'] = 'offline'
 
 OmegaConf.register_new_resolver("add", lambda x, y: int(x) + int(y))
 
