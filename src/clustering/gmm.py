007890aa8d535c9709182b60c08d42cb460bf11f
nadjarutsch <nadja.rutsch@gmx.de>
2023-03-23 17:43:23 +0100

    code cleanup

--- src/clustering/gmm.py
+++ src/clustering/gmm.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import torch
 import numpy as np
 from sklearn.mixture import GaussianMixture
@@ -15,10 +17,11 @@ class GMM(GaussianMixture):
     Attributes:
         labels_: Cluster labels, size M x 1.
         memberships_: One-hot encoded cluster memberships, size M x n_clusters.
+        attributes of the GaussianMixture class by scikit-learn
     """
 
     def __init__(self, n_clusters: int, random_state: int):
-        """Initializes the instance from given data.
+        """Initializes an instance of the GMM with the specified number of components.
 
         Args:
             n_clusters: Number of mixture components used to fit the Gaussian Mixture Model.
@@ -29,15 +32,15 @@ class GMM(GaussianMixture):
         self.labels_ = None
         self.memberships_ = None
 
-    def fit(self, data: torch.Tensor, y: None = None):
+    def fit(self, data: torch.Tensor, y: None = None) -> GMM:
         """Estimates model parameters with the EM algorithm and predicts cluster labels from the learned mixture model.
 
         Args:
             data: Datapoints used to learn the model, size M x D.
-            y: Not used, present for API consistency by convention (scikit-learn).
+            y: Not used, present for API consistency with scikit-learn.
 
         Returns:
-             self: The fitted mixture.
+             self: The fitted mixture model.
         """
 
         super().fit(data, y)
--- src/clustering/kmeans.py
+++ src/clustering/kmeans.py
@@ -1,18 +1,48 @@
-from sklearn.cluster import KMeans
-import numpy as np
-from clustering.utils import *
+from __future__ import annotations
 
+import torch
+import numpy as np
+from sklearn.cluster import KMeans
+
+from clustering.utils import labels_to_one_hot
 
 
 class Kmeans(KMeans):
-    def __init__(self, init, n_clusters, soft=False):
+    """k-means clusterer from scikit-learn with extended fit method.
+
+    The fit method of the scikit-learn class is extended such that one-hot encoded cluster memberships are stored in an
+    instance and can be accessed as an attribute.
+
+    Attributes:
+        memberships_: One-hot encoded cluster memberships, size M x n_clusters.
+        attributes of the Kmeans class by scikit-learn, e.g. labels_
+    """
+
+    def __init__(self, init: str, n_clusters: int):
+        """Initializes an instance of the k-means clusterer as specified.
+
+       Args:
+           init: Method used for initializing the cluster centers.
+           n_clusters: Number of clusters used to fit the k-means clusterer.
+       """
         super().__init__(init=init, n_clusters=n_clusters)
-        self.soft = soft
+
+        self.memberships_ = None
 
     # extended method
-    def fit(self, data):
-        super(Kmeans, self).fit(data)
-        if self.soft:
-            pass
-        else:
-            self.memberships_ = labels_to_one_hot(self.labels_[self.labels_ >= 0], np.max(self.labels_) + 1)
+    def fit(self, data: torch.Tensor, y: None = None, sample_weight: None = None) -> Kmeans:
+        """Estimates cluster means with the EM algorithm and assigns datapoints to each cluster.
+
+        Args:
+            data: Datapoints used to learn the model, size M x D.
+            y: Not used, present for API consistency with scikit-learn.
+            sample_weight: Not used, present for API consistency with scikit-learn.
+
+        Returns:
+             self: The fitted k-means clusterer.
+        """
+        super().fit(data)
+
+        self.memberships_ = labels_to_one_hot(self.labels_[self.labels_ >= 0], np.max(self.labels_) + 1)
+
+        return self
